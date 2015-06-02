8c0de34a3eb4a069bbb277902fc85bbcc042f170
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-06-02 12:11:05 -0600

    debug me with postman

--- src/main/scala/servlets.scala
+++ src/main/scala/servlets.scala
@@ -16,13 +16,48 @@ class MyServlet extends HttpServlet {
     val responseBody: NodeSeq =
       <html>
         <body>
-          <h1>Hello, world!</h1>
+          <h1>whatever</h1>
+          <p>This is an app that checks for auth headers</p>
+          <p>Get PostMan for google chrome to debug it</p>
+          <h2>To do list:</h2>
+          <ul>
+            <li>Figure out routing. This is currently just a catch all.</li>
+            <li>create a branch for simple JSON webservices boilerplate</li>
+          </ul>
         </body>
       </html>
 
     val printer = new PrettyPrinter(80, 2)
 
     response.getWriter.write(printer.formatNodes(responseBody))
+
+  }
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse) {
+
+    
+    response.setContentType("application/json")
+    response.setCharacterEncoding("UTF-8")
+
+
+    val headers = request.getAuthType()
+    val query = request.getQueryString()
+    val url = request.getRequestURL()
+    val authorization = request.getHeader("Authorization")
+
+    val printer = new PrettyPrinter(80, 2)
+
+    response.setStatus(201)
+    response.getWriter.write(
+      "Hello, your auth header is " +
+        headers +
+        " and your query string is " +
+        query +
+        " and you are on " +
+        url  +
+        ". The authorization code you have is " +
+        authorization)
+
 
   }
 
